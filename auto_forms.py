db4603ada6354f98b7be020e6085cacbc34e1b69
Matheus Henrique <matheus.ptes@gmail.com>
2024-08-15 16:05:49 -0300

    Atualização do script para converter arquivo em pdf

--- auto_forms.py
+++ auto_forms.py
@@ -1,25 +1,29 @@
 import pandas as pd
 from docxtpl import DocxTemplate
 from pathlib import Path
+from docx2pdf import convert
 
 # Função para formatar o CPF
 def formatar_cpf(cpf):
     cpf_str = str(cpf).zfill(11)  # Converter para string e garantir que tenha 11 dígitos com zeros à esquerda
     return f"{cpf_str[:3]}.{cpf_str[3:6]}.{cpf_str[6:9]}-{cpf_str[9:]}"
 
+
+
 # Ler a planilha
-planilha = pd.read_excel("Dados.xlsx")
+planilha = pd.read_excel("Teste.xlsx")
 
 # Dicionário de templates
 templates = {
-    'RH NOSSA': Path("C:/Users/meidelvein.ps/Downloads/Auto/FORM_NOSSA.docx"),
-    'CWBEM': Path("C:/Users/meidelvein.ps/Downloads/Auto/FORM_CWBEM.docx"),
-    'FOUR HANDS': Path("C:/Users/meidelvein.ps/Downloads/Auto/FORM_FOUR_HANDS.docx")
+    'RH NOSSA': Path("C:/Users/mathe/Downloads/Auto/FORM_NOSSA.docx"),
+    'CWBEM': Path("C:/Users/mathe/Downloads/Auto/FORM_CWBEM.docx"),
+    'FOUR HANDS': Path("C:/Users/mathe/Downloads/Auto/FORM_FOUR_HANDS.docx")
 }
 
 
 # Caminho da pasta onde os formulários serão salvos
-pasta_caminho = Path("C:/Users/meidelvein.ps/Downloads/FORMULARIOS/TESTES")
+pasta_caminho_docx = Path("C:/Users/mathe/Downloads/FORMULARIOS/TESTES")
+pasta_caminho_pdf = Path("C:/Users/mathe/Downloads/FORMULARIOS/PDFs")
 
 # Iterar sobre as linhas da planilha
 for index, row in planilha.iterrows():
@@ -44,11 +48,12 @@ for index, row in planilha.iterrows():
                 }
 
                 template.render(context)
-                caminho_arq = pasta_caminho / f'{identificador}.docx'
+                caminho_arq = pasta_caminho_docx / f'{identificador}.docx'
 
                 # Salvar o documento
                 template.save(caminho_arq)
                 print(f"Documento salvo: {caminho_arq}")
+                convert(pasta_caminho_docx, pasta_caminho_pdf)
 
             except Exception as e:
                 print(f"Erro ao processar o template para a empresa {empresa}: {e}")
